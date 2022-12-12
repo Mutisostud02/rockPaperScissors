8a1cb3d4f5f990897e5346413a77c3de6ab16537
Mutisostud02 <mutiso8395@gmail.com>
2022-12-13 00:47:10 +0300

    Added function to get player input and play it against the computer in the web console.

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
 function getComputerChoice(){
    const randomChoice=Math.floor(Math.random()*3)+1;
    if(randomChoice==1){
-      return 'Rock';
+      return 'rock';
    }
    else if(randomChoice==2){
-      return 'Paper';
+      return 'paper';
    }
-   else if(randomChoice==3){
-      return 'Scissors';
+   else{
+      return 'scissors';
    }
 }
+
+function player(){
+   let ansr= prompt("Select your rock-paper-scissor choice:");
+   return ansr.toLowerCase();
+}
+
+function playRound(playerSelection,computerSelection){
+  if(playerSelection=='rock' && computerSelection=='paper'){
+  return console.log("you lose!Paper beats rock");
+  }
+  if(playerSelection=='rock' && computerSelection=='scissors'){
+   return console.log("you win!rock beats scissors");
+   }
+   if(playerSelection=='paper' && computerSelection=='rock'){
+   return console.log("you win!Paper beats rock");
+   }
+   if(playerSelection=='paper' && computerSelection=='scissors'){
+   return console.log("you lose!scissors beats paper");
+   }
+   if(playerSelection=='scissors' && computerSelection=='paper'){
+   return console.log("you win!scissors beats paper");
+   }
+   if(playerSelection=='scissors' && computerSelection=='rock'){
+   return console.log("you lose!rock beats scissors");
+   }
+   if(playerSelection=='rock' && computerSelection=='rock'){
+   return alert("Play again!");
+   }if(playerSelection=='paper' && computerSelection=='paper'){
+   return alert("Play again!");
+   }
+   if(playerSelection=='scissors' && computerSelection=='scissors'){
+   return alert("Play again!");
+   }
+
+
+
+}
+console.log(playRound(player(),getComputerChoice()));
