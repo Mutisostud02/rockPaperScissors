a5a70745f8f71a2ac2ba7243b956dc32c9171d40
Mutisostud02 <mutiso8395@gmail.com>
2022-12-19 17:30:30 +0300

    added game function that plays the game 5 times added userScore and compScore to collect scores returned winner of the 5 rounds.

--- script.js
+++ script.js
@@ -15,36 +15,80 @@ function player(){
    let ansr= prompt("Select your rock-paper-scissor choice:");
    return ansr.toLowerCase();
 }
+let fail;//for everytime I lose ,to be used to give game results
+let win//for every time I win
+let compScore=0;
+let userScore=0;
+
 
 function playRound(playerSelection,computerSelection){
+
   if(playerSelection=='rock' && computerSelection=='paper'){
-  return console.log("you lose!Paper beats rock");
+   compScore++;
+  return "you lose!Paper beats rock";
+  
   }
   if(playerSelection=='rock' && computerSelection=='scissors'){
-   return console.log("you win!rock beats scissors");
+   userScore++;
+   return "you win!rock beats scissors";
+   
    }
    if(playerSelection=='paper' && computerSelection=='rock'){
-   return console.log("you win!Paper beats rock");
+      userScore++;
+   return "you win! Paper beats rock";
+   
    }
    if(playerSelection=='paper' && computerSelection=='scissors'){
-   return console.log("you lose!scissors beats paper");
+      compScore++;
+   return "you lose! scissors beats paper";
+ 
    }
    if(playerSelection=='scissors' && computerSelection=='paper'){
-   return console.log("you win!scissors beats paper");
+      userScore++;
+   return "you win! scissors beats paper";
+
    }
    if(playerSelection=='scissors' && computerSelection=='rock'){
-   return console.log("you lose!rock beats scissors");
+      compScore++;
+   return "you lose! rock beats scissors";
+   
    }
    if(playerSelection=='rock' && computerSelection=='rock'){
-   return alert("Play again!");
+   return "Its a tie!";
    }if(playerSelection=='paper' && computerSelection=='paper'){
-   return alert("Play again!");
+   return "Its a tie!";
    }
    if(playerSelection=='scissors' && computerSelection=='scissors'){
-   return alert("Play again!");
+   return "Its a tie!";
    }
-
-
-
+   if(playerSelection==''){
+      compScore++;
+      return "you lose! Invalid answer!";
+      
+     
+   }
+   if(playerSelection!='scissors' && playerSelection!='paper' && playerSelection!='rock'){
+      compScore++;
+      return "you lose! Incorrect choice!";
+      
+   }
 }
-console.log(playRound(player(),getComputerChoice()));
+let arr=[];
+
+function game(){
+   for(let i=1;i<6;i++){
+   console.log((playRound(player(),getComputerChoice())));
+
+   }
+console.log("computer score is: " + compScore);
+console.log("user score is : " + userScore);
+if(compScore>userScore){
+   console.log("GAME OVER!! You Have lost");
+}else if(compScore<userScore){
+   console.log("CONGRATULATIONS!! You Have Won")
+}else{
+   console.log("GAME OVER!! No Winner, Reload to Play Again");
+}
+}
+
+game();
